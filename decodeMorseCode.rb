701e96a21c8b8301c0354353cdbe724eb1ed97ad
tYohannes <teklegy@gmail.com>
2022-03-30 12:58:00 +0300

    Fix linter errors

--- decodeMorseCode.rb
+++ decodeMorseCode.rb
@@ -1,44 +1,42 @@
-$morse_character_code = {
-  A: '.-',
-  B: '-...',
-  C: '-.-.',
-  D: '-..',
-  E: '.',
-  F: '..-.',
-  G: '--.',
-  H: '....',
-  I: '..',
-  J: '.---',
-  K: '-.-',
-  L: '.-..',
-  M: '--',
-  N: '-.',
-  O: '---',
-  P: '.--.',
-  Q: '--.-',
-  R: '.-.',
-  S: '...',
-  T: '-',
-  U: '..-',
-  V: '...-',
-  W: '.--',
-  X: '-..--',
-  Y: '-.--',
-  Z: '--..',
-  1 => '.----',
-  2 => '..---',
-  3 => '...--',
-  4 => '....-',
-  5 => '.....',
-  6 => '-....',
-  7 => '--...',
-  8 => '---..',
-  9 => '----.',
-  0 => '-----',
-}
-
 def decode_char(char)
-  $morse_character_code.key(char).to_s
+  morse_character_code = {
+    A: '.-',
+    B: '-...',
+    C: '-.-.',
+    D: '-..',
+    E: '.',
+    F: '..-.',
+    G: '--.',
+    H: '....',
+    I: '..',
+    J: '.---',
+    K: '-.-',
+    L: '.-..',
+    M: '--',
+    N: '-.',
+    O: '---',
+    P: '.--.',
+    Q: '--.-',
+    R: '.-.',
+    S: '...',
+    T: '-',
+    U: '..-',
+    V: '...-',
+    W: '.--',
+    X: '-..--',
+    Y: '-.--',
+    Z: '--..',
+    1 => '.----',
+    2 => '..---',
+    3 => '...--',
+    4 => '....-',
+    5 => '.....',
+    6 => '-....',
+    7 => '--...',
+    8 => '---..',
+    9 => '----.',
+    0 => '-----'
+  }.key(char).to_s
 end
 
 def decode_word(word_in_morse)
@@ -56,10 +54,10 @@ def decode(message)
   last_word = message.split('   ').last
 
   message.split('   ').each do |word|
-    unless word === last_word
-      message_in_ascii += decode_word(word).to_s + ' '
-    else
+    if word == last_word
       message_in_ascii += decode_word(word).to_s
+    else
+      message_in_ascii += "#{decode_word(word)} "
     end
   end
 
